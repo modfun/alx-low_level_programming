007eca450439c95a7e41bc2b8363093f3be0af4a
modfun <modfun95@gmail.com>
2022-09-09 21:31:05 +0200

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens, ones;
+
+	for (tens = 48; tens < 58; tens++)
+	{
+		for (ones = 48; ones < 58; ones++)
+		{
+			if (tens < ones)
+			{
+				putchar(tens);
+				putchar(ones);
+
+				if (tens != 56 && ones <= 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		if (tens != 56 && ones < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		
+	}
+	putchar('\n');
+
+	return (0);
+}
