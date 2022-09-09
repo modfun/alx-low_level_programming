878f5904b2fe34912818e9b82548943d8e740789
modfun <modfun95@gmail.com>
2022-09-09 21:11:46 +0200

    print comb 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char c;
+	int c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = (int)'0'; c <= (int)'9'; c++)
 	{
 		putchar(c);
-		if (c != '9')
+		if (c != (int)'9')
 		{
 			putchar(',');
 			putchar(' ');
