8b6b334a8dfe32e9d5bf8cf59d293d7392e93e0d
modfun <modfun95@gmail.com>
2022-09-09 20:53:29 +0200

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
