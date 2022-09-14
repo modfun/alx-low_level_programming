05f6e65ddef228662fbcecd8937fb68cbf1a6be1
modfun <modfun95@gmail.com>
2022-09-14 09:41:44 +0200

    sign 2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,17 @@
 #include "main.h"
+
 /**
- * print_sign - Print the sign of a number
- * @n: type int integer, can be negative or positive
- * Description: print +, 0, or - depending on number, along with return
- * Return: 1 if +, 0 if 0, and -1 if -
+ * print_sign - print the sign of a number (+, -)
+ * @n: an integer number
+ *
+ * Return: one, zero or negative one
  */
 int print_sign(int n)
 {
 	if (n > 0)
-	{
-		_putchar('+');
 		return (1);
-	}
 	else if (n < 0)
-	{
-		_putchar('-');
 		return (-1);
-	}
 	else
-	{
-		_putchar('0');
 		return (0);
-	}
 }
