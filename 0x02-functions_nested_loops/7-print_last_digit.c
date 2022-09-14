14ef8879fe5d298fd9e0e62e1823f6364e087522
modfun <modfun95@gmail.com>
2022-09-14 10:37:30 +0200

    print last digit 3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,9 @@
 int print_last_digit(int n)
 {
 	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+		n = -1 * (n % 10);
+	else
+		n = n % 10;
+	_putchar(n + '0');
+	return (n);
 }
