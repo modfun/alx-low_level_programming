5f5be6ec53c23c9182bc502ba494024bf836fa19
modfun <modfun95@gmail.com>
2022-09-14 17:48:39 +0200

    fibfibfib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,37 +7,31 @@
  */
 int main(void)
 {
-
 	unsigned long int fnum_1 = 0, bnum_1 = 1, fnum_2 = 0, bnum_2 = 2;
-	unsigned long int tmp_f, tmp_b, tmp;
-	int i;
+	unsigned long int tmp1, tmp2, tmp3;
 	const unsigned long int BIG = 10000000000;
+	int i;
 
-	printf("%lu, %lu", bnum_1, bnum_2);
-
+	printf("%lu, %lu, ", bnum_1, bnum_2);
 	for (i = 2; i < 98; i++)
 	{
-		if (fnum_1 > 0 || fnum_2 > 0 || (bnum_2	+ bnum_1) > BIG)
+		if (bnum_1 + bnum_2 > BIG || fnum_2 > 0 || fnum_1 > 0)
 		{
-			tmp_f = (bnum_2	+ bnum_1) / BIG;
-			tmp_b = (bnum_2 + bnum_1) % BIG;
-			tmp = tmp_f + (fnum_2 + fnum_1);
-			fnum_1 = fnum_2;
-			fnum_2 = tmp;
-
-			bnum_1 = bnum_2;
-			bnum_2 = tmp_b;
-
-			printf(", %lu%lu", fnum_2, bnum_2);
+			tmp1 = (bnum_1 + bnum_2) / BIG;
+			tmp2 = (bnum_1 + bnum_2) % BIG;
+			tmp3 = fnum_1 + fnum_2 + tmp1;
+			fnum_1 = fnum_2, fnum_2 = tmp3;
+			bnum_1 = bnum_2, bnum_2 = tmp2;
+			printf("%lu%010lu", fnum_2, bnum_2);
 		}
 		else
 		{
-			tmp = bnum_2 + bnum_1;
-			bnum_1 = bnum_2;
-			bnum_2 = tmp;
-
-			printf(", %lu", bnum_2);
+			tmp2 = bnum_1 + bnum_2;
+			bnum_1 = bnum_2, bnum_2 = tmp2;
+			printf("%lu", bnum_2);
 		}
+		if (i != 97)
+			printf(", ");
 	}
 
 	printf("\n");
