78c794062f07173b9faaaa1f68035e56cdaa401b
modfun <modfun95@gmail.com>
2022-09-14 13:27:07 +0200

    fib 4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <inttypes.h>
+
+#define LARGEST 10000000000
 
 /**
  * main - print 98 fibonacci sequenece numbers
@@ -8,22 +9,32 @@
  */
 int main(void)
 {
-	uint64_t i, j, k, fib;
 
-	j = 1;
-	k = 2;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	printf("%"PRIu64", %"PRIu64"", j, k);
-
-	for (i = 0; i < 96; i++)
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		fib = k + j;
-		j = k;
-		k = fib;
-
-		printf(", %"PRIu64"", fib);
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
-
 	printf("\n");
 
 	return (0);
