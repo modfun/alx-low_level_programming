61c90e603f7cfa6d94f581f39dbd9abb99636f74
modfun <modfun95@gmail.com>
2022-09-15 15:17:29 +0200

    Cleaning up 0x04

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    print_number(1245254);
+    _putchar('\n');
+    print_number(-92345438);
+    _putchar('\n');
+    print_number(1000000000);
+    _putchar('\n');
+
+    return (0);
+}
