c6d91701559ad1678329788be694b1bda3fe4d30
modfun <modfun95@gmail.com>
2022-09-20 15:12:26 +0200

    restto98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-* reset_to_98 -  takes a pointer to an int
-* @n: take an int
-* Return: int
-**/
-
+ * reset_to_98 - make an int 98
+ * @n: a pointer to an int type variable
+ *
+ * Description: a function that takes a pointer to an int as parameter and 
+ *              updates the value it points to to 98.
+ */
 void reset_to_98(int *n)
 {
-	*n = 98;
+  *n = 98;
 }
