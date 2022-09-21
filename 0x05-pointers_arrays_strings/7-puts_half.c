34b934a635a5eeaaada113bcfe2f927e4967f6e9
modfun <modfun95@gmail.com>
2022-09-21 08:38:50 +0200

    putshalf

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,45 +1,27 @@
 #include "main.h"
 
 /**
-* _strlen - get the length of a string.
-* @s: a pointer to a char
-* Return: length of a string
-**/
-
-int _strlen(char *s)
-{
-int l, i = 0;
-
-while (s[i] != '\0')
-{
-i++;
-}
-l = i;
-return (l);
-}
-
-/**
-* puts_half -  prints half of a string
-* @str: a pointer to string
-* Return: string
-**/
-
+ * puts_half - print half
+ * @str: a pointer to string
+ *
+ * Description: a function that prints half of a string, followed by
+ * a new line.
+ */
 void puts_half(char *str)
 {
+	int count = 0;
 	int i;
-	int size = _strlen(str);
 
-	if (size % 2 != 0)
+	while (*(str + count) != '\0')
 	{
-		i = (size / 2) + 1;
+		count++;
 	}
-	else
+
+	for (i = count / 2; i < count; i++)
 	{
-		i = size / 2;
-	}
-	for (i = i; i < size; i++)
-	{
-		_putchar(str[i]);
+		_putchar(*(str + i));
 	}
 	_putchar('\n');
 }
+
+
