21562316c3f7ffbd684f671062a97f5d613d4138
modfun <modfun95@gmail.com>
2022-09-22 10:07:59 +0200

    main.c

---
+++ 0x06-pointers_arrays_strings/_strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - get the length of string
+ * @s: a pointer to string
+ *
+ * Description: a function to return a strings length
+ * Return: an int value
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _putchar(char c);
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+
+#endif
