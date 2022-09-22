80ab4e8b49a1924974c1ff38d779214d6bbf0ed3
modfun <modfun95@gmail.com>
2022-09-22 17:35:59 +0200

    capstring

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * cap_string - captitalize all words in string
+ * @str: a poniter to string
+ *
+ * Description: a function that capitalizes all words of a string.
+ * Separators: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }
+ * Return: a pointer to string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		switch (str[i])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (str[i + 1] != 0)
+				{
+					if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+						str[i + 1] = str[i + 1] + ('A' - 'a' - 0);
+				}
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
+
+	return (str);
+}
