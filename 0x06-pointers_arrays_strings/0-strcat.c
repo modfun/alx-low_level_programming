6ae763b578bcabeb5bd18c0049d2e99cd55378ce
modfun <modfun95@gmail.com>
2022-09-22 10:09:04 +0200

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate a string
+ * @dest: a pointer to the destination string
+ * @src: a pointer to the source string
+ *
+ * Description: This function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest, and
+ * then adds a terminating null byte.
+ * Return: a pointer to the destination string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destCount = _strlen(dest);
+	int srcCount = _strlen(src);
+	int i;
+
+	for (i = destCount; i < (destCount + srcCount); i++)
+	{
+		dest[i] = src[i - destCount];
+	}
+
+	return (dest);
+}
