a7fb7523a7c3eefd852d27c3db3da19c1fb7090c
modfun <modfun95@gmail.com>
2022-09-23 16:57:38 +0200

    printbuffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,38 +1,51 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
-* print_buffer - prints a buffer.
-* @b: pointer to string
-* @size: total number of bytes
-* Return: nothing
-**/
-
+ * print_buffer - print a buffer to stdout
+ * @b: a pointer to string
+ * @size: the size to be printed
+ *
+ * Description: a function that prints a buffer.
+ */
 void print_buffer(char *b, int size)
 {
 	int c, d;
 
-	c = 0;
 	if (size <= 0)
-	{
 		printf("\n");
-	}
 
+	c = 0;
 	while (c < size)
 	{
-		printf("%8.8x:", c);
+		printf("%8.8x: ", c);
 		d = 0;
 		while (d < 10)
 		{
-			printf("%02x", b[c + d]);
-			if ((d % 2 == 0 && d != 0) || (c + d > size - 1))
-			{
+			if (c + d < size)
+				printf("%02x", b[c + d]);
+			else
+				printf("  ");
+
+			if (d % 2 != 0)
 				printf(" ");
+			d++;
+		}
+		d = 0;
+		while (d < 10)
+		{
+			if (c + d < size)
+			{
+				if (b[c + d] < ' ')
+					printf(".");
+				else
+					printf("%c", b[c + d]);
 			}
 			d++;
 		}
-		c += 10;
 		printf("\n");
-	}
 
+		c += 10;
+	}
 }
