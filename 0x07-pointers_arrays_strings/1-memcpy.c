d43072e28d617d70213a071424765f1cfd1e434d
modfun <modfun95@gmail.com>
2022-09-27 14:31:07 +0200

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
-* _memcpy - a function that copies memory area
-* @dest: char
-* @src: char
-* @n: int
-*
-* Return: a pointer to dest
-**/
+ * _memcpy - copy memory area
+ * @dest: destination memory area
+ * @src: source memory area
+ * @n: number of bytes to copy
+ *
+ * Description: a function that copies memory area.
+ * Return: a pointer memory area dest.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n ; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
+
 	return (dest);
 }
