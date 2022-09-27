74fbb5e65a18f1373aa7e0a961879bdd763319a5
modfun <modfun95@gmail.com>
2022-09-27 15:14:05 +0200

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,24 +1,26 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
-* _strpbrk - function searches string for any set of bytes
-* @s: string
-* @accept: string
-*
-* Return: a pointer to the byte in s
-**/
+ * _strpbrk - search a string
+ * @s: a string
+ * @accept: a set to search for
+ *
+ * Description: a function that searches a string for any of a set of bytes.
+ * Return: a pointer to found member of accept of NULL.
+ */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != 0; j++)
 		{
 			if (s[i] == accept[j])
 				return (s + i);
 		}
 	}
-	return (0);
+
+	return (NULL);
 }
