3e86dc6d84547f1ab1278444e9b430926c807188
modfun <modfun95@gmail.com>
2022-09-29 14:45:40 +0200

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
-* _pow_recursion - returns the value of x raised to the power of y
-* @x: integer
-* @y: integer
-* Return: integer
-**/
-
+ * _pow_recursion - the power function
+ * @x: integer base
+ * @y: integer exponent
+ *
+ * Description: a function to return the power of x raised by y.
+ * Return: a positive integer.
+ */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if ((x == 1) || (y == 0))
+	if (y == 0)
 		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
