4b3909b817217254e3ee2d249118066282db222e
modfun <modfun95@gmail.com>
2022-09-29 14:25:34 +0200

    print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,18 @@
 #include "main.h"
-/**
-* _print_rev_recursion - a function that prints a string in reverse
-* @s: pointer to array of char
-**/
 
+/**
+ * _print_rev_recursion - print revesivly recursivly
+ * @s: a pointer to string
+ *
+ * Description: a function to print in reverse a string recursivly
+ */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == 0)
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
