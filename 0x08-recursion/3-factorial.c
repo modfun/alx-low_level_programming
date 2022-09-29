8791388bdc12537485cd909086daa3a1b501c60f
modfun <modfun95@gmail.com>
2022-09-29 14:39:10 +0200

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,7 +9,10 @@
  */
 int factorial(int n)
 {
-	if (n < 0) return (-1);
-	if (n == 0) return (1);
-	else return (n * factorial(n - 1));
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
