909e8e22890aec63f67a7d1cf62889f69a836926
modfun <modfun95@gmail.com>
2022-10-01 11:32:04 +0200

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,50 +1,53 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
-* main - entry point
-* @argc: size of argv
-* @argv: array
-* Return: 0
-**/
-int main(int argc, char *argv[])
+ * main - Entry Point
+ * @argc: count of arguments
+ * @argv: vector of arguments
+ *
+ * Description: prints the minimum number of coins to make change for
+ *an amount of money. usage: ./change cents
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
 {
-	int cent, i;
+	int money, coins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cent = atoi(argv[1]);
-	i = 0;
-	while (cent > 0)
+	money = atoi(argv[1]);
+	while (money > 0)
 	{
-		if (cent >= 25)
+		if (money >= 25)
 		{
-			cent -= 25;
-			i += 1;
+			coins += money / 25;
+			money = money % 25;
 		}
-		else if (cent >= 10)
+		else if (money >= 10)
 		{
-			cent -= 10;
-			i += 1;
+			coins += money / 10;
+			money = money % 10;
 		}
-		else if (cent >= 5)
+		else if (money >= 5)
 		{
-			cent -= 5;
-			i += 1;
+			coins += money / 5;
+			money = money % 5;
 		}
-		else if (cent >= 2)
+		else if (money >= 2)
 		{
-			cent -= 2;
-			i += 1;
+			coins += money / 2;
+			money = money % 2;
 		}
-		else
+		else if (money >= 1)
 		{
-			cent -= 1;
-			i += 1;
+			coins++;
+			money--;
 		}
 	}
-	printf("%d\n", i);
+	printf("%d\n", coins);
 	return (0);
 }
