2bd7b534387b6a6719e6ea25d49a64cea05b802d
modfun <modfun95@gmail.com>
2022-10-06 17:28:57 +0200

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,54 +15,37 @@ char *getNbytes(char *s2, unsigned int n);
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
-	char *constr;
+	int l, i;
+	unsigned int j;
+	char *p;
 
-	if (s1 == NULL && s2 == NULL)
+	l = 0;
+	i = 0;
+	j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[l])
+		l += 1;
+
+	p = malloc(l + n + 1);
+	if (p == NULL)
+		return (NULL);
+
+	while (s1[i++])
 	{
-		constr = malloc(1);
-		if (constr == NULL)
-			return (NULL);
-		constr[1] = '\0';
-		return (constr);
+		p[i] = s1[i];
+		i += 1;
 	}
-	else if (s1 == NULL)
-		return (n < strlen(s2) ? getNbytes(s2, n) : getNbytes(s2, strlen(s2)));
-	else if (s2 == NULL)
-		return (getNbytes(s1, strlen(s1)));
 
-	if (n > strlen(s2))
-		n = strlen(s2);
-	constr = malloc(strlen(s1) + n + 1);
-	if (constr == NULL)
-		return (NULL);
-	for (i = 0; s1[i] != '\0'; i++)
-		constr[i] = s1[i];
-	for (i = 0; i < n; i++)
-		constr[strlen(s1) + i] = s2[i];
-	constr[strlen(s1) + n] = '\0';
+	while (j < n)
+	{
+		p[i + j] = s2[j];
+		j += 1;
+	}
+	p[i + j] = '\0';
 
-	return (constr);
-}
-
-/**
- * getNbytes - get (n) number of bytes from string
- * @s: a pointer to string
- * @n: number of bytes
- *
- * Return: a pointer to string.
- */
-char *getNbytes(char *s, unsigned int n)
-{
-	unsigned int i;
-	char *ptr;
-
-	ptr = malloc(n + 1);
-	if (ptr == NULL)
-		return (NULL);
-	for (i = 0; i < n; i++)
-		ptr[i] = s[i];
-	ptr[i] = '\0';
-
-	return (ptr);
+	return (p);
 }
