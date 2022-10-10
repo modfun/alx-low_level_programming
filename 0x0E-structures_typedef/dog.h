2526dc51a576ed8f2b758a1fbf2857c984e75076
modfun <modfun95@gmail.com>
2022-10-10 20:16:06 +0200

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - woof woof
+ * @name: a pointer to string
+ * @age: puppy or doggy
+ * @owner: hooman name
+ *
+ * Description: dogs are better than cats.
+ */
 struct dog
 {
 	char *name;
