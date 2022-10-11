79f50c0ca25e8e09cec9fe9af09922a689bef27d
modfun <modfun95@gmail.com>
2022-10-11 09:04:33 +0200

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
-* struct dog - dog data
-* @name: name of dog
-* @age: age of dog
-* @owner: name of owner dogs
-* Description: Defines metadata about the dog and type of dogs
-**/
-typedef struct dog
+ * struct dog - Woof Woof!
+ * @name: a string to name doggy
+ * @age: age of fluffiness
+ * @owner: name of the Hooman!
+ *
+ * Description: doggy or puppy all are welcome!
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-/* Additional function prototypes */
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+};
+
 #endif
