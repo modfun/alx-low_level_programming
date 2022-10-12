fc3d13f6d0dc8c662f6a538c310c903c3a6bfc15
modfun <modfun95@gmail.com>
2022-10-12 21:40:26 +0200

    simple_operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,9 +21,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 
 	i = 0;
-	while (i < 6)
+	while (i < 5)
 	{
-		if (s[0] == (ops[i]).op[0])
+		if (s[0] == (ops[i]).op[0] && s[0] != 0)
 			return ((ops[i]).f);
 		i++;
 	}
