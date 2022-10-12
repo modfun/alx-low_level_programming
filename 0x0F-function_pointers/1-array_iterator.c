2af7299333d7ea1f238b10cbba2e314878e4c407
modfun <modfun95@gmail.com>
2022-10-12 20:08:05 +0200

    array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates over an array
+ * @array: an array of integers
+ * @size: array size
+ * @action: iterator of signature void (*f)(int)
+ *
+ * Description: a function that executes a function given as a parameter on
+ * each element of an array.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
+	}
+}
