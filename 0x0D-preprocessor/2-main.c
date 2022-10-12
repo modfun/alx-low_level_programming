1e9a95faa2b93ed488ecc49efc33658a97a2a496
modfun <modfun95@gmail.com>
2022-10-12 16:19:05 +0200

    main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,16 @@
+#include <stdlib.h>
 #include <stdio.h>
+
 /**
-* main - prints the name of the file it was compiled from
-*
-* Return: always 0
-**/
+ * main - Entry Point
+ *
+ * Description: a program that prints the name of the file it was compiled
+ * from, followed by a new line.
+ * Return: Always zero (Success)
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
+
 	return (0);
 }
