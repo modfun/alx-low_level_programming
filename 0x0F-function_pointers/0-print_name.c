10010a65feffe5e29e2d853264162418c58fb6c8
modfun <modfun95@gmail.com>
2022-10-12 19:55:19 +0200

    print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - name print
+ * @name: a string
+ * @f: a pointer to function with signature void f(char *)
+ *
+ * Description: a function that prints a name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
